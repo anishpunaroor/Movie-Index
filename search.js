02b13999d5d0659f8827e5375f3e111888c3f102
Anish Punaroor <anish.punaroor@gmail.com>
2021-08-31 13:30:38 -0700

    Init API Access

---
+++ search.js
@@ -0,0 +1,6 @@
+// Use TMDB API
+
+const API_KEY = 'api_key=a262fb1998a2c64c05a5d5e7a7635772'; 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+
