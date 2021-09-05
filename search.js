00691bf83ebe2f4d83a117a471ef05db807dcb31
Anish Punaroor <anish.punaroor@gmail.com>
2021-09-05 00:39:38 -0700

    Genre id's

--- search.js
+++ search.js
@@ -6,6 +6,85 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY; 
 
+const genres = [
+    {
+        "id":28,
+        "name":"Action"
+    },
+    {
+        "id":12,
+        "name":"Adventure"
+    },
+    {
+        "id":16,
+        "name":"Animation"
+    },
+    {
+        "id":35,
+        "name":"Comedy"
+    },
+    {
+        "id":80,
+        "name":"Crime"
+    },
+    {
+        "id":99,
+        "name":"Documentary"
+    },
+    {
+        "id":18,
+        "name":"Drama"
+    },
+    {
+        "id":10751,
+        "name":"Family"
+    },
+    {
+        "id":14,
+        "name":"Fantasy"
+    },
+    {
+        "id":36,
+        "name":"History"
+    },
+    {
+        "id":27,
+        "name":"Horror"
+    },
+    {
+        "id":10402,
+        "name":"Music"
+    },
+    {
+        "id":9648,
+        "name":"Mystery"
+    },
+    {
+        "id":10749,
+        "name":"Romance"
+    },
+    {
+        "id":878,
+        "name":"Science Fiction"
+    },
+    {
+        "id":10770,
+        "name":"TV Movie"
+    },
+    {
+        "id":53,
+        "name":"Thriller"
+    },
+    {
+        "id":10752,
+        "name":"War"
+    },
+    {
+        "id":37,
+        "name":"Western"
+    }
+]
+
 const main = document.getElementById('main'); 
 const form = document.getElementById('form'); 
 const tags = document.getElementById('tags');
